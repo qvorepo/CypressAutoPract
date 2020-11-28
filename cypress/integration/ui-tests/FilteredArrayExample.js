9ebdc747b757ee1c18b6216a1591f6b6b47a0395
qvorepo <48115228+qvorepo@users.noreply.github.com>
2020-11-28 14:52:47 -0800

    Update the filteredArrayExample test.

---
+++ cypress/integration/ui-tests/FilteredArrayExample.js
@@ -0,0 +1,44 @@
+/// <reference types="Cypress" />
+
+//const { has, hasIn } = require("cypress/types/lodash");
+
+describe('Filtered Array Example', () => {
+    it('Filtered Array Example', () => {
+     const items=[
+       {name: 'Bike', price: 100},
+       {name: 'TV', price: 200},
+       {name: 'Album', price: 10},
+       {name: 'Book', price: 5},
+       {name: 'Phone', price: 500},
+       {name: 'Computer', price: 1000},
+       {name: 'Keyboard', price: 25}
+
+     ];
+
+     const filteredItems=items.filter((item)=>{
+       return item.price <=100;
+     });
+
+    
+     cy.log(JSON.stringify(items));
+     cy.log(JSON.stringify(filteredItems))
+
+     const album=items.filter((item)=>{
+      return item.name ==='Album';
+    });
+    cy.log(JSON.stringify(album));
+
+    const hasInexpensiveItems=items.some((item)=>{
+      return item.price <=100;
+    });
+    cy.log(hasInexpensiveItems);
+
+    const hasExpensiveItems=items.every((item)=>{
+      return item.price >=3000;
+    });
+    cy.log(hasExpensiveItems);
+         
+
+    }) 
+
+  })
