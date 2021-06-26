1eb861fd6dea89cb6a3090776acbfeb66368b88c
Quang Vo <qtvo91@yahoo.com>
2021-06-26 12:37:08 -0700

    Add a Google search and validate contents in a drop down list.

--- cypress/integration/ui-tests/DropdownSelectTest.js
+++ cypress/integration/ui-tests/DropdownSelectTest.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 describe('Dropdown Select Example', () => {
-    it('Dropdown Select Test', () => {
+    it.skip('Dropdown Select Test', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/');
      
       cy.get('#txtUsername').clear().type('Admin');
@@ -10,6 +10,16 @@ describe('Dropdown Select Example', () => {
       cy.get('#menu_directory_viewDirectory').click();
       cy.get('#searchDirectory_job_title').select('HR Manager');// This is the essense of the test/example.
          
+
+    }) 
+
+    it('Google Search for cypress', () => {
+      cy.visit('https://www.google.com/');
+     
+      cy.get('input[name="q"').type('Cypress')
+      cy.get('.erkvQe')
+      .find('li div span')
+      .contains('cypress testing')
 
     }) 
 
